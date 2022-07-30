bb72ce78d1db5d194f232a875d159496bfee8a31
Eduardo <eddvelez@outlook.com>
2022-07-30 15:51:33 -0500

    change index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Header from '../src/components/header'
-import Link from 'next/link'
-import Image from 'next/image'
-import ChoreCard from '../src/components/chore-card';
-import { createAgreement } from '../src/service/create-agreement'
+import { Input, Button } from '@geist-ui/core'
+import createUSer from '../src/service/create-user'
+import Router from 'next/router'
 
 const HomeContainer = styled.main`
   padding: 0rem 0rem;
@@ -17,8 +15,11 @@ const HomeContainer = styled.main`
     color: #5D6768;
   }
   & > .page-content {
-    padding: 0rem 2rem;
+    padding: 2rem 2rem;
     overflow: auto;
+    width: 100%;
+    max-width: 1120px;
+    margin: 0 auto;
   }
 
   & .agreement-list {
@@ -30,75 +31,26 @@ const HomeContainer = styled.main`
 }
 `
 
-const FooterAction = styled.div`
-  background-color: #374487;
-  color: white;
-  min-height: 64px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-radius: 1.5rem 1.5rem 0rem 0rem;
-  padding: 1rem;
-  & .plus-link {
-    display: flex;
-    flex-direction: column;
-    color: white;
-    text-align: center;
-  }
-  & .plus {
-    background-color: #FFEF14;
-    border: none;
-    width: 82px;
-    height: 82px;
-    border-radius: 41px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  padding-bottom: 64px;
-`
-
-
-
 export default function Home () {
-  
-  const onSubmit = () => {
-    
+  const [user, setUser] = useState('')
+  const onSubmit = async () => {
+    const payload = { username: user }
+    const response = await createUSer(payload)
+    console.log(response)
+    localStorage.setItem('user', response.user.username)
+    localStorage.setItem('id', response.user._id)
+    Router.push('/add-home')
   }
-
-  return (
+  
+ return (
   <HomeContainer>
     <Header>EVEN LIVING</Header>
     <section className='page-content'>
-      <div className='agreement-list'>
-        <ChoreCard
-          name='Clo'
-          picture='/f.svg'
-          agreement='Take out the trash'
-          recurrence='1 times a week'
-        />
-        <ChoreCard
-          name='Eduardo'
-          picture='/m.svg'
-          agreement='Take out the trash'
-          recurrence='1 times a week'
-        />
-      </div>
-      <p className='empty-state'>This is your agreement list: Click on the button <b>Add</b> below to add a new agreement.</p>  
+    <Input value={user} onChange={e => setUser(e.target.value)} width="100%" placeholder="username">Username</Input>
+    <Button onClick={() => onSubmit()} classNametype='success'>Start with username</Button>
     </section>
     <footer>
-      <FooterAction>
-        <Link href='/agreements/new'>
-        <a className='plus-link'>
-        <div className="plus">
-          <Image src='/plus.svg' width='41' height='41'/>
-        </div>
-        New agreement
-        </a>
-        </Link>
-      </FooterAction>
     </footer>
   </HomeContainer>
-  )
+ )
 }
